7252f8c157dafd71cb1ff76e8ff4f09aa9ee754a
Clara Weber <88279664+claraweber@users.noreply.github.com>
2022-10-30 21:04:34 +0100

    Add files via upload

---
+++ precache-manifest.05f59f824f82b10f9d744d7f91a1ac12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25bfc6ca90191e47bd14044964ff2e9",
+    "url": "/u-dicom-viewer/index.html"
+  },
+  {
+    "revision": "782169710f96751e7341",
+    "url": "/u-dicom-viewer/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "edf841668af093c4c8d4",
+    "url": "/u-dicom-viewer/static/css/main.fa5b3062.chunk.css"
+  },
+  {
+    "revision": "782169710f96751e7341",
+    "url": "/u-dicom-viewer/static/js/2.2a4a2ea6.chunk.js"
+  },
+  {
+    "revision": "aacc489f352063e28b11373bca46c134",
+    "url": "/u-dicom-viewer/static/js/2.2a4a2ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf841668af093c4c8d4",
+    "url": "/u-dicom-viewer/static/js/main.4fd200c7.chunk.js"
+  },
+  {
+    "revision": "4e9954b4f9bc47440a7d",
+    "url": "/u-dicom-viewer/static/js/runtime-main.1153c50b.js"
+  }
+]);
